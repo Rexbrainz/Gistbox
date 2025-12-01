a4cf82e4331a60839e7200fbd2d3ea2a71c43ebb
Success Daniel <sudaniel@2-D-6.42heilbronn.de>
2025-12-01 10:31:42 +0100

    change app name to gistbox

--- gistbox/cmd/web/routes.go
+++ gistbox/cmd/web/routes.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func (app *application) routes() *http.ServeMux {
-	mux := http.ServeMux()
+	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
